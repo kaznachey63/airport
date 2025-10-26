db463264eba0cf9be643aa01bb624a18b46e58e0
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-10-26 15:08:54 +0300

    добавил: файл типа самолета

---
+++ AirportApp/Models/TypeOfAircraft.cs
@@ -0,0 +1,28 @@
+﻿namespace AirportApp.Models
+{
+    /// <summary>
+    /// Тип самолета
+    /// </summary>
+    public enum TypeOfAircraft
+    {
+        /// <summary>
+        /// Пусто
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Боинг
+        /// </summary>
+        Boieng,
+
+        /// <summary>
+        /// Аэрбас
+        /// </summary>
+        Airbus,
+
+        /// <summary>
+        /// Объединенная авиастроительная корпорация
+        /// </summary>
+        UnitedAircraftCorporation
+    }
+}
