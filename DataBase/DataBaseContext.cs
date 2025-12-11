3a1ea2f493a0202da94980e1e487649c240922d9
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-12-11 18:42:07 +0300

    добавлено: бдКонтекст и бдСторадж

---
+++ DataBase/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using AirportApp.Constants;
+using AirportApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataGridView.DataBaseStorage
+{
+    public class DataBaseContext : DbContext
+    {
+        /// <summary>
+        /// Сущность <see cref="FlightModel"/>.
+        /// </summary>
+        public DbSet<FlightModel> Flights { get; set; }
+
+        /// <summary>
+        /// Создаёт экземпляр <see cref="DataBaseContext"/>.
+        /// </summary>
+        public DataBaseContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=AirportDB;Trusted_Connection=True;");
+    }
+}
