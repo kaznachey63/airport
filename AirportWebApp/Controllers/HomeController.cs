d4d5c1a6fe4e9146f01c392373b9d09f41ef7ebc
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-12-14 19:57:44 +0300

    настроено: контроллер под главную стр., стр. добавить и пустые для редакта добавлено: саммари в контроллере

--- AirportWebApp/Controllers/HomeController.cs
+++ AirportWebApp/Controllers/HomeController.cs
@@ -1,28 +1,98 @@
 using AirportWebApp.Models;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 using System.Diagnostics;
 
 namespace AirportWebApp.Controllers
 {
+    /// <summary>
+    /// Контроллер главной страницы и операций с рейсами
+    /// </summary>
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IFlightService flightService = default!;
 
-        public HomeController(ILogger<HomeController> logger)
+        /// <summary>
+        /// Конструктор с логгером
+        /// </summary>
+        public HomeController(IFlightService FlightService)
         {
-            _logger = logger;
+            flightService = FlightService;
         }
 
+        /// <summary>
+        /// Главная страница со списком рейсов
+        /// </summary>
         public IActionResult Index()
+        {
+            return View(new FlightIndexViewModel());
+        }
+
+        /// <summary>
+        /// Страница добавления рейса
+        /// </summary>
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new FlightEditViewModel());
+        }
+
+        /// <summary>
+        /// Добавление рейса
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Add(FlightEditViewModel model, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Маппим ViewModel в модель домена
+            var flight = new FlightModel
+            {
+                Id = Guid.NewGuid(),
+                FlightNumber = model.FlightNumber,
+                TypeOfAircraft = model.TypeOfAircraft,
+                ArrivalTime = model.ArrivalTime,
+                NumberOfPassengers = model.NumberOfPassengers,
+                PassengerFee = model.PassengerFee,
+                CrewNumber = model.CrewNumber,
+                CrewFee = model.CrewFee,
+                ServicePercentage = model.ServicePercentage
+            };
+
+            await flightService.Add(flight, cancellationToken);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// Страница редактирования рейса
+        /// </summary>
+        public IActionResult Update()
         {
             return View();
         }
 
+        /// <summary>
+        /// Страница удаления рейса
+        /// </summary>
+        public IActionResult Remove()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Страница конфиденциальности
+        /// </summary>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Страница ошибки
+        /// </summary>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
