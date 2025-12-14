3406d1f7aac16063bfa03824b809b450b40a73e8
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-12-14 19:30:25 +0300

    добавлено: модели

---
+++ AirportWebApp/Models/FlightStatisticsViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace AirportWebApp.Models
+{
+    /// <summary>
+    /// Статистика по рейсам
+    /// </summary>
+    public class FlightStatisticsViewModel
+    {
+        /// <summary>
+        /// Общее количество рейсов
+        /// </summary>
+        public int TotalFlights { get; set; }
+
+        /// <summary>
+        /// Общее количество пассажиров
+        /// </summary>
+        public int TotalPassengers { get; set; }
+
+        /// <summary>
+        /// Общее количество членов экипажа
+        /// </summary>
+        public int TotalCrew { get; set; }
+
+        /// <summary>
+        /// Общая сумма сборов по всем рейсам
+        /// </summary>
+        public decimal TotalRevenue { get; set; }
+    }
+}
